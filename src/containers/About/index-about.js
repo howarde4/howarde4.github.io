173fc180493f257592a9a4ff25696c64f3f09710
Emi <emi.howard4@gmail.com>
2022-05-27 13:19:14 -0400

    updated about

--- src/containers/About/index-about.js
+++ src/containers/About/index-about.js
@@ -26,16 +26,16 @@ const About = () => {
         <Box style={classes.contain}>
             <Box style={classes.circle}>
 
-                <Box sx={{ mt: 6, mr: 6, ml: 6 }}>
+                <Box sx={{ mt: 5, mr: 6, ml: 6 }}>
                     <Typography variant="description">About Me</Typography>
                     <br />
                     <br />
                     <Typography variant="smallPrint">
-                        {<p>Hi there! Thank you for visiting my page. My name is Emi and I am a software engineer specializing in front and full stack development.
-                            I am based in Brooklyn, New York but can work remotely anywhere! My journey with software engineering began during the pandemic when I
-                            decided to spend my time during quarantine learning a new skill. I started to learn simple coding and fell in love with it! I decided to
-                            do a software engineering bootcamp and have since learned the ins and outs of engineering. I am proficient in Javascript and Python and
-                            love to work with React, React Native, Express and Postgres. I can build web and mobile applications and would love to learn more to
+                        {<p>Hi there! My name is Emi and I am a software engineer specializing in front and full stack development.
+                            I am based in Brooklyn, New York but open to relocating or working remotely anywhere! My journey with software engineering began during
+                            the pandemic when I decided to spend my time during quarantine learning a new skill. I started to learn simple coding and fell in love
+                            with it! I decided to do a software engineering bootcamp and have since learned the ins and outs of engineering. I am proficient in Javascript
+                            and Python and love to work with React, React Native, Express and Postgres. I can build web and mobile applications and would love to learn more to
                             become a better programmer.</p>}
                     </Typography>
                 </Box>
